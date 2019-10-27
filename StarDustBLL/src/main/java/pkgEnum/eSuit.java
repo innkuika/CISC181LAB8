7285a4bc90062035118e7ee2472bb75bd2e5a523
tstba <tstba@DESKTOP-T9JUA1D.fios-router.home>
2019-10-27 11:41:20 -0400

    added suit character

--- StarDustBLL/src/main/java/pkgEnum/eSuit.java
+++ StarDustBLL/src/main/java/pkgEnum/eSuit.java
@@ -1,5 +1,20 @@
 package pkgEnum;
 
 public enum eSuit {
-	HEARTS, CLUBS, DIAMONDS, SPADES, JOKER;
+	HEARTS((char)'\u2764'), CLUBS((char)'\u2663'), 
+	DIAMONDS((char)'\u2666'), SPADES((char)'\u2660'), JOKER((char)'\uDCCF');
+	
+	private char iSuitChar;
+	
+	private eSuit(char SuitChar)
+	{
+		this.iSuitChar = SuitChar;
+	}
+
+	public char getiSuitChar() {
+		return iSuitChar;
+	}
+
+	
+	
 }
